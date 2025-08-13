f4f3fbc045e8175059b56934c269b4440f42649d
Domesoul <udoekongud@gmail.com>
2025-08-13 14:55:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,6 @@
-// Minimal SW; forces update on each deploy by versioning (?v=7) and skipWaiting
-const CACHE_NAME = "meal-planner-v7";
+// sw.js
+const CACHE_NAME = "meal-planner-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
-
-self.addEventListener("install", (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-});
-self.addEventListener("activate", (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
-});
+self.addEventListener("install", e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS))); });
+self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
