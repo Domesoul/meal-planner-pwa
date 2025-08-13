41b66e70dd83bd4573e365456af6dc7165c5cb0c
Domesoul <udoekongud@gmail.com>
2025-08-13 10:05:39 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+
+const CACHE_NAME = 'meal-planner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
